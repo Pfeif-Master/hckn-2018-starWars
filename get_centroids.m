e7cebcef33e606018fe92f2510868ade15ca3d60
Hunter Pfeiffer <whpfeiff@umich.edu>
2018-07-14 14:28:43 -0700

    FIX: crash on blob lost

--- get_centroids.m
+++ get_centroids.m
@@ -1,4 +1,4 @@
-function [cm_upper_1, cm_upper_2, cm_lower_1, cm_lower_2] = get_centroids(raw_RGB)
+function [valid, cm_upper_1, cm_upper_2, cm_lower_1, cm_lower_2] = get_centroids(raw_RGB)
 
 [bw_orange, bw_purple] = get_O_P(raw_RGB);
 
@@ -6,17 +6,37 @@ function [cm_upper_1, cm_upper_2, cm_lower_1, cm_lower_2] = get_centroids(raw_RG
 cm_orange = regionprops(bw_orange, 'centroid');
 cm_purple = regionprops(bw_purple, 'centroid');
 
-o = cm_orange.Centroid;
-p = cm_purple.Centroid;
+%% check if found blob
+so = size(cm_orange);
+sp = size(cm_purple);
+foundO = so(1);
+foundP = sp(1);
+
+if(foundO)
+    o = cm_orange.Centroid;
+end
+if(foundP)
+    p = cm_purple.Centroid;
+end
+
+cm_upper_1 = 0;
+cm_upper_2 = 0;
+cm_lower_1 = 0;
+cm_lower_2 = 0;
+valid = 0;
+
 
 % cm_upper_1 = floor(o(1));
 % cm_upper_2 = floor(o(2));
 % cm_lower_1 = floor(p(1));
 % cm_lower_2 = floor(p(2));
-cm_upper_1 = o(1);
-cm_upper_2 = o(2);
-cm_lower_1 = p(1);
-cm_lower_2 = p(2);
+if(foundO && foundP)
+    valid = 1;
+    cm_upper_1 = o(1);
+    cm_upper_2 = o(2);
+    cm_lower_1 = p(1);
+    cm_lower_2 = p(2);
+end
 
 end
 
--- testAndScraps/centroid.m
+++ testAndScraps/centroid.m
@@ -1,9 +1,11 @@
-clear;
 addpath('Filters\');
 
 %% get centroids
-raw = imread('Media\scraps\green.jpg');
-[u1, u2, L1, L2] = get_centroids(raw);
+% raw = imread('Media\green\Image226.jpg');
+raw = oldFrame;
+[o, p] = get_O_P(raw);
+bw = o | p;
+[v, u1, u2, L1, L2] = get_centroids(raw);
 
 %% plot
 figure;
@@ -11,4 +13,7 @@ imshow(raw);
 hold on
 text(u1, u2, 'T');
 text(L1, L2, 'B');
+
+figure;
+imshow(bw);
 
