f18f4324d9ecb7e0b172084146e12e490e26e28f
Hunter Pfeiffer <whpfeiff@umich.edu>
2018-07-14 19:57:36 -0700

    FIX: plasma legnth

--- lightsaber.m
+++ lightsaber.m
@@ -1,32 +1,52 @@
 function Image = lightsaber(img,Ux, Uy, Lx, Ly)
 
-% p2x = 550;
-% p2y = 700;
-% p1x = 500;
-% p1y = 600;
 
-dx = Ux - Lx;
+dx = abs(Ux - Lx);
 if dx == 0
     dx = 1; %no neg infinty please
 end
-dy = Uy - Ly;
+dy = abs(Uy - Ly);
 d = pdist([Ux, Uy; Lx, Ly]);
 
 slope = dy/dx;
+
+
+%% get sign of slope
+if Uy > Ly
+    if Ux > Lx
+        slope = slope;
+    else
+        slope = slope * -1;
+    end
+else
+    if Ux > Lx
+        slope = -slope;
+    else
+        slope = slope;        
+    end
+end
+    
 b = Uy - Ux * slope;
-%p2x = p2x -10;
 
+%% set sizes
 s = size(img);
-
 h = 20; % hight
 w = 10; % width
-for i = 0:d
-    if slope > 0
+mult = 3; %plasma scale factor
+
+%% scale plasma legth
+HorAng = atan2(dy, dx);
+xstend = mult * d * cos(HorAng);
+
+for i = 0:xstend
+    %% x directions
+    if Ux < Lx
         Nx = Ux - i;
     else
         Nx = Ux + i;
     end
     
+    %% bound check
     if Nx - w <= 0
         break;
     end
@@ -42,6 +62,8 @@ for i = 0:d
     if Ny - h <= 0
         break;
     end
+    
+    %% draw line
     img([Ny - h : Ny + h],[Nx - w : Nx + w] , :) = 255; 
 end
 
