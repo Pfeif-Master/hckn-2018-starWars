a7938a8bcb4a97e3864bf3fcb34d0081d13c9a66
Hunter Pfeiffer <whpfeiff@umich.edu>
2018-07-15 01:56:08 -0700

    Final form of render

--- Render.m
+++ Render.m
@@ -1,16 +1,21 @@
 clear;
 addpath('Filters\filterSet2\');
 
-frameDivider = 4;
+frameDivider = 1;
 
-vr = VideoReader('Media\starwars\Edited_Lightsaber_Shots_raw.mp4');
-vw = VideoWriter('Media\starwars\short.mp4');
+vr = VideoReader('Media\starwars\tech_demo_1\Raw_test_data1.MP4');
+
+vw = VideoWriter('Media\starwars\tech_demo_1\wesley');
 vw.FrameRate = vr.FrameRate / frameDivider;
 open(vw);
 
+vw_cv = VideoWriter('Media\starwars\tech_demo_1\wesley_cv');
+vw_cv.FrameRate = vr.FrameRate / frameDivider;
+open(vw_cv);
+
 i = 0;
 while hasFrame(vr)
-%while i < 60
+% while i < 400
     q = false;
     for k = 1 : frameDivider - 1
         if ~hasFrame(vr)
@@ -36,20 +41,26 @@ while hasFrame(vr)
     end
     
     [v, u1, u2, L1, L2] = get_centroids(oldFrame);
+    
+    cv_Frame = stage1(oldFrame);
+    
     if(v)
         newFrame = insertText(oldFrame, [u1, u2], 'U');
         newFrame = insertText(newFrame, [L1, L2], 'L');
         newFrame = lightsaber(newFrame, floor(u1), floor(u2), floor(L1), floor(L2));
+        
     end
 %     imwrite(oldFrame, ['Media\debug\raw\Image' int2str(i), '.jpg']);
 %     imwrite(newFrame, ['Media\debug\out\Image' int2str(i), '.jpg']);
 %     imshow(newFrame);
     writeVideo(vw, newFrame);
+    writeVideo(vw_cv, cv_Frame);
     
 
     i = i + 1
 end
 
 close(vw);
+close(vw_cv);
 
 
